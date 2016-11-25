abb792ae1fc278d5e6ae2f0a6a376819a5d048a8
camila <camomr@gmail.com>
2016-11-25 17:14:53 -0300

    Mi app Django Girls, primer commit

---
+++ 6.13/ejer1.py
@@ -0,0 +1,13 @@
+volumen = 57
+if volumen < 20:
+	print ("It's kinda quiet")
+elif 20 <= volumen <40:
+	print ("It's nice for backgroun music")
+elif 40 <= volumen <60:	
+	print("Perfect, I can hear all the details")
+elif 60 <= volumen <80:
+	print ("Nice for parties")
+elif 80 <= volumen <100:
+	print ("A bit loud!")
+else:
+	print ("My ears are hurting! :(")
